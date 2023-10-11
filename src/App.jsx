5ad583051bd0dfafbba451832e8d9c5f41e29b23
suyog <shresthasuyog001@gmail.com>
2023-10-11 10:29:14 +0545

    routing test

--- src/App.jsx
+++ src/App.jsx
@@ -22,17 +22,17 @@ function App() {
 
           <Routes>
  
-            <Route path="https://seeyog-stha.github.io/Company/" element={<Hero/>}/>
+            <Route path="/Company/" element={<Hero/>}/>
             <Route element={<Layout setTheme={setTheme}></Layout>}>
 
-            <Route path="https://seeyog-stha.github.io/Company/Home" element={<Home />}></Route>
-            <Route path="https://seeyog-stha.github.io/Company/Staff" element={<StaffPage />}></Route>
-            <Route path="https://seeyog-stha.github.io/Company/Contact" element={<Contact/>}/>
-            <Route path="https://seeyog-stha.github.io/Company/About" element={<About/>}/>
+            <Route path="/Company/Home" element={<Home />}></Route>
+            <Route path="/Company/Staff" element={<StaffPage />}></Route>
+            <Route path="/Company/Contact" element={<Contact/>}/>
+            <Route path="/Company/About" element={<About/>}/>
             </Route>
-            <Route path="https://seeyog-stha.github.io/Company/Staff/:id" element={<StaffDetails/>}></Route>
-            <Route path="https://seeyog-stha.github.io/Company/Staff/Create" element={<Create></Create>}></Route>
-            <Route path="https://seeyog-stha.github.io/Company/Staff/Update/:id" element={<Update></Update>}></Route>
+            <Route path="/Company/Staff/:id" element={<StaffDetails/>}></Route>
+            <Route path="/Company/Staff/Create" element={<Create></Create>}></Route>
+            <Route path="/Company/Staff/Update/:id" element={<Update></Update>}></Route>
           </Routes>
           
         </BrowserRouter>
