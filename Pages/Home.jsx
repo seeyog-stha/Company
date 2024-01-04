4f7043e033dd0f41ee388e1e733f17687d43e582
seeyog-stha <shresthasuyog001@gmail.com>
2024-01-04 10:57:16 +0545

    blogs adjustments

--- Pages/Home.jsx
+++ Pages/Home.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Carousel from '../src/component/Carousel'
 import Company from '../src/component/Company'
-
+import Blogs from "../src/component/Blogs"
 export default function Home() {
   return (
     <div>
       <Carousel/>
       <Company/>
+      <Blogs></Blogs>
     </div>
   )
 }
--- src/component/Blogs.jsx
+++ src/component/Blogs.jsx
@@ -34,40 +34,71 @@ export default function Blogs() {
       <h4 className="text-3xl text-center font-bold underline">
         Our Latest Blogs
       </h4>
-      <div className="mx-3 my-7  h-[50vh] flex justify-evenly">
-        {BlogsData.map((obj) => (
-          <div
-            className="w-1/5 h-full  rounded-tr-xl shadow-xl shadow-gray-500 bg-gray-300"
-            key={obj.id}
+      {pending&&
+      <div className="flex justify-center">
+
+        <span className="loading loading-dots loading-lg "></span>
+      </div>
+      }
+      {error && (
+        <div role="alert" className="alert alert-error">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="stroke-current shrink-0 h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
           >
-            <div className="w-full h-2/3 flex items-center justify-center p-1">
-              <img
-                src={obj.img}
-                alt="img"
-                className="w-full border rounded-tr-xl h-full"
-              />
-            </div>
-            <div className="flex items-center w-full h-[25%] ">
-              <p className="w-[95%] h-full  text-center text-xl overflow-hidden line-clamp-3 text-ellipsis  ">
-                {obj.head}
-              </p>
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+            />
+          </svg>
+          <span>{error}</span>
+        </div>
+      )}
+      {value && (
+        <div>
+          <div className="mx-3 my-7  h-[50vh] flex justify-evenly">
+            {BlogsData.map((obj) => (
+              <div
+                className="w-1/5 h-full  rounded-tr-xl shadow-xl shadow-gray-500 bg-gray-300"
+                key={obj.id}
+              >
+                <div className="w-full h-2/3 flex items-center justify-center p-1">
+                  <img
+                    src={obj.img}
+                    alt="img"
+                    className="w-full border rounded-tr-xl h-full"
+                  />
+                </div>
+                <div className="flex items-center w-full h-[25%] ">
+                  <p className="w-[95%] h-full  text-center text-xl overflow-hidden line-clamp-3 text-ellipsis  ">
+                    {obj.head}
+                  </p>
+                </div>
+              </div>
+            ))}
+          </div>
+          <div className="flex justify-center py-5">
+            <div className="join grid grid-cols-2 w-1/6">
+              <button
+                className="join-item btn btn-outline"
+                onClick={handelPageDown}
+              >
+                Previous page
+              </button>
+              <button
+                className="join-item btn btn-outline"
+                onClick={handelPageUp}
+              >
+                Next
+              </button>
             </div>
           </div>
-        ))}
-      </div>
-      <div className="flex justify-center py-5">
-        <div className="join grid grid-cols-2 w-1/6">
-          <button
-            className="join-item btn btn-outline"
-            onClick={handelPageDown}
-          >
-            Previous page
-          </button>
-          <button className="join-item btn btn-outline" onClick={handelPageUp}>
-            Next
-          </button>
         </div>
-      </div>
+      )}
     </>
   );
 }
